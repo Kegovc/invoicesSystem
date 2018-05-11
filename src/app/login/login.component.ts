b8732bad64ee3c058dd1cc523d02e2206803a021
Octavio Villavicencio Cruz <Octavio.Villavicencio@GFMEGA.COM>
2018-05-11 12:07:39 -0500

    inicio formularios reactivos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { StateKey } from '@angular/platform-browser';
 import { environment } from './../../environments/environment';
 import { Router } from '@angular/router';
 import { AuthService } from './../shared/services/auth.service';
 import { UserService } from '../shared/services/user.service';
-import { debug } from 'util';
+import { FormBuilder, FormGroup, FormControl, Validators  } from '@angular/forms';
 
 
 
@@ -19,15 +18,26 @@ export class LoginComponent implements OnInit {
 
   public data: any = {
     rfc: 'vico940131sk8'
-};
+  };
+  public loginForm: FormGroup;
+  public errorMessages: any = {};
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
   }
+
+  createForm() {
+    this.loginForm = this.formBuilder.group( {
+      key: ['', Validators.required],
+      rfc: ['', [Validators.required, Validators.pattern('\[a-zA-z]{3,4}')]]
+    });
+  }
+
   doLongin() {
     if (environment.debug) { console.log( 'DEBUG : ', this.data ); }
 
@@ -39,16 +49,46 @@ export class LoginComponent implements OnInit {
 
   }
 
-  doValidate() {
-    this.userService.login(this.data)
-    .then(response => {
-      console.log('DEBUG : ', response);
-      return true;
-    })
-    .catch(error => {
-      console.error('Error : ', error);
+  hasError(controlName, error, force = false) {
+    const control = this.loginForm.controls[controlName];
+
+    try {
+      return control.errors[error] && (control.dirty || force);
+    } catch (e) {
       return false;
-    });
-    return false;
+    }
+  }
+
+
+  showMessages() {
+    for (const k in this.loginForm.controls) {
+      if (this.loginForm.controls.hasOwnProperty(k)) {
+        this.errorMessages[k] = this.hasError(k, 'required', true);
+      }
+    }
+  }
+
+
+  doValidate() {
+
+    console.log('Form: ', this.loginForm);
+    this.showMessages();
+    if (!this.loginForm.valid) {
+      console.log('Error: ', this.errorMessages);
+      return;
+    }
+    // tslint:disable-next-line:one-line
+    else {
+      this.userService.login(this.data)
+      .then(response => {
+        console.log('DEBUG : ', response);
+        return true;
+      })
+      .catch(error => {
+        console.error('Error : ', error);
+        return false;
+      });
+      return false;
+    }
   }
 }
